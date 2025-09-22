a0dadfd5d732ad700e41b91794e5e0c92f9da0f3
Matthieu43120 <matthieu.alexander@outlook.fr>
2025-09-22 11:21:54 +0200

    Update Deno std version to 0.190.0

--- supabase/functions/create-user-profile/index.ts
+++ supabase/functions/create-user-profile/index.ts
@@ -1,5 +1,5 @@
 ```typescript
-import { serve } from 'https://deno.land/std@0.210.0/http/server.ts'
+import { serve } from 'https://deno.land/std@0.190.0/http/server.ts'
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.52.1'
 
 const corsHeaders = {
